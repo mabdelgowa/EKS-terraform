e0e4afe880ee8cc1ab5d1884e3a1ceb27f00d610
alexGradProj2023 <safety.box.graduation.project@gmail.com>
2023-07-26 05:15:03 +0300

    last

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,3 +1,15 @@
+provider "kubernetes" {
+  load_config_file = "false"
+  host = data.aws_eks_cluster.myapp-cluster
+  token = data.aws_eks_cluster_auth.myapp-cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.myapp-cluster.certificate_authority.0.data)
+}
+data "aws_eks_cluster""myapp-cluster"{
+  name = module.eks.cluster_id
+}
+data "aws_eks_cluster_auth" "myapp-cluster"{
+  name = module.eks.cluster_id
+}
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.15.3"
